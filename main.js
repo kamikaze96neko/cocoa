9843700306ba86706fec7a4c3c1ca897e5e185ec
kamikaze96neko <kamikaze96neko@yahoo.co.jp>
2014-12-05 16:18:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
-var milkcocoa = new MilkCocoa("https://io-xi3154o5o.mlkcca.com");
-/* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
-var chatDataStore = milkcocoa.dataStore("chat");
+ var milkcocoa = new MilkCocoa("https://io-hi1wslbu4.mlkcca.com"); 
+ /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */ 
+ var chatDataStore = milkcocoa.dataStore("chat"); 
+ 
+ 
+ // フォームの情報を記録しておく変数 
+ var textArea, board,textName;                    
+ 
+ 
+ // ページが読み込み終わったときの処理 
+ window.onload = function(){ 
+   textArea = document.getElementById("msg"); 
+   board = document.getElementById("board"); 
+   textName = document.getElementById("txtName");  
+ } 
+ 
+ 
+ function clickEvent(){ 
+   var text = textArea.value; 
+   sendText(text,textName.value);                  
+ } 
+ 
+ 
+ function sendText(text,nm){                       
+   chatDataStore.push({uname : nm,content : text},function(data){  
+     console.log("送信完了!"); 
+     textArea.value = ""; 
+   }); 
+ } 
+ 
+ 
+ chatDataStore.on("push",function(data){ 
+   addText(data.value);     // ←これ 
+}); 
 
-var textArea, board,textName; 
-
-window.onload = function(){
-  textArea = document.getElementById("msg");
-  board = document.getElementById("board");
-  textName = document.getElementById("txtName")
-}
-
-function clickEvent(){
-  var text = textArea.value;
-  sendText(text,textName,value);
-  sendText(text);
-}
-
-function sendText(text,nm){
-  chatDataStore.push({uname : nm,content:text},function(data){
-    console.log("送信完了!");
-    textArea.value = "";
-  });
-}
-
-chatDataStore.on("push",function(data){
-  addText(data.value);
-});
-
-function addText(text){
-  var msgDom = document.createElement("li");
-  msgDom.innerHTML = text.uname+":"+text.content;
-  board.insertBefore(msgDom, board.firstChild);
-}
+ 
+ function addText(text){ 
+  var msgDom = document.createElement("li"); 
+  msgDom.innerHTML = text.uname+":"+text.content; // ←これ 
+  board.insertBefore(msgDom, board.firstChild); 
+  
+} 
