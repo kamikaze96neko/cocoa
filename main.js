f2cd282920f76e8bd8bc9630c25624dac6d52715
kamikaze96neko <kamikaze96neko@yahoo.co.jp>
2014-12-05 15:39:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var milkcocoa = new MilkCocoa("https://io-xi3154o5o.mlkcca.com");
-var milkcocoa = new MilkCocoa("https://{ your-app-id }.mlkcca.com/");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
 var chatDataStore = milkcocoa.dataStore("chat");
 var textArea, board;
